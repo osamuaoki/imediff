6fbdc9ed3a4d1f2087aca8e1776e5d7117eff5fa
Osamu Aoki <osamu@debian.org>
2021-06-28 16:12:58 +0900

    Modern Python packaging with src/
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # vi:se ts=4 sts=4 et ai:
 """
-Copyright (C) 2018       Osamu Aoki <osamu@debian.org>
+Copyright (C) 2018-2021       Osamu Aoki <osamu@debian.org>
 
 This is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as
@@ -18,39 +18,5 @@ License along with the program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """
 import setuptools
-from DistUtilsExtra.command import *
-from imediff import VERSION, PACKAGE
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name=PACKAGE,
-    version=VERSION,
-    author="Jarno Elonen, Osamu Aoki",
-    author_email="elonen@iki.fi, osamu@debian.org",
-    description="Interactive Merge Editor for DIFF2/3",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/osamuaoki/imediff",
-    packages=setuptools.find_packages(),
-    scripts=["bin/git-ime"],
-    entry_points={"console_scripts": ["imediff=imediff:main"]},
-    data_files=[
-        ("share/man/man1", ["doc/imediff.1", "doc/git-ime.1"]),
-        ("lib/git-core/mergetools", ["mergetools/imediff"]),
-    ],
-    test_suite="test.test_diff23lib",
-    license="GPLv2+",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "Environment :: Console :: Curses",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Text Processing :: General",
-    ],
-    cmdclass={"build": build_extra.build_extra, "build_i18n": build_i18n.build_i18n},
-)
+setuptools.setup()
