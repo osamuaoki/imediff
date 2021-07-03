16d21089b5afb072ff03e89a6c174768ddcc44e2
Osamu Aoki <osamu@debian.org>
2021-07-03 22:33:38 +0900

    Fix transition error
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- src/imediff/__init__.py
+++ src/imediff/__init__.py
@@ -23,4 +23,4 @@ License along with this program; if not, write to the Free
 Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 Boston, MA 02110-1301, USA.
 """
-from imediff.main import *
+from imediff.__main__ import *
